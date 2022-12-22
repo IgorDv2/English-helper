5f3390505199452cb7f8bf45ff34f9070e47fa47
IgorDv2 <121171901+IgorDv2@users.noreply.github.com>
2022-12-22 15:00:15 +0400

    Update Source.cpp

--- English helper/Source.cpp
+++ English helper/Source.cpp
@@ -9,12 +9,12 @@
 using namespace std;
 
 int main() {
-	SetConsoleCP(1251);
-	SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);				//эти две строчки нужны для отображения
+	SetConsoleOutputCP(1251);		//русских букв в консоли Windows
 
 	CoreTesting case1;
-	case1.FileToArray();
-	case1.PhrasalTesting();
+	case1.FileToArray();			//Подготавливает строчки из файла для тестирования
+	case1.PhrasalTesting();			//Начинает тестирование, запрашивает тип тестирования
 	
 
 	return 0;
